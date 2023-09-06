6302bd3263e0ea7d39d95a56149d95d61624a2b6
hdhakaita <78197458+hdhakaita@users.noreply.github.com>
2023-09-06 17:44:58 +0530

    Update streamlit_app.py
    added header and text to page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parent New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.test('Hi This is Hemendra Dhakaita')
+streamlit.test('Learning Snowflake App development')
