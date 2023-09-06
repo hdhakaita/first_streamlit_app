f509d55ba06fd54741aa7a86805e2d6b823ff9e0
hdhakaita <78197458+hdhakaita@users.noreply.github.com>
2023-09-06 19:01:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,10 @@ import streamlit
 streamlit.title('My parent New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Hi This is Hemendra Dhakaita')
-streamlit.text('Learning Snowflake App development')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+--streamlit.header('Breakfast Menu')
+--streamlit.text('Hi This is Hemendra Dhakaita')
+--streamlit.text('Learning Snowflake App development')
