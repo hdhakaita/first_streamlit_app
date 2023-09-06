29d4135b662d4626952b9d66967cbf0ab683c771
hdhakaita <78197458+hdhakaita@users.noreply.github.com>
2023-09-06 19:03:26 +0530

    Update streamlit_app.py
    added icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit
 streamlit.title('My parent New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
---streamlit.header('Breakfast Menu')
---streamlit.text('Hi This is Hemendra Dhakaita')
---streamlit.text('Learning Snowflake App development')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑 🍞 Hard-Boiled Free-Range Egg')
+   
+#streamlit.header('Breakfast Menu')
+#--streamlit.text('Hi This is Hemendra Dhakaita')
+#--streamlit.text('Learning Snowflake App development')
