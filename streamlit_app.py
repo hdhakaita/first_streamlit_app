e53abc5755606d8950611aaaf6f34e6f38d6b5f3
hdhakaita <78197458+hdhakaita@users.noreply.github.com>
2023-09-06 19:05:16 +0530

    Update streamlit_app.py
    added another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑 🍞 Hard-Boiled Free-Range Egg')
    
-#streamlit.header('Breakfast Menu')
+#--streamlit.header('Breakfast Menu')
 #--streamlit.text('Hi This is Hemendra Dhakaita')
 #--streamlit.text('Learning Snowflake App development')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
