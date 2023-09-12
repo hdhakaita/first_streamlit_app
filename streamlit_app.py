fd911a840faed0594dddc7d9bc77697a38aa7223
hdhakaita <78197458+hdhakaita@users.noreply.github.com>
2023-09-12 18:16:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,11 @@ streamlit.text('🥑 🍞 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+# my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit_list = streamlit.multiselect("Pick some fruits:", list(my_fruit_list),  ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
 streamlit.dataframe(my_fruit_list)
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
@@ -16,8 +19,6 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 # Display the table on the page.
 
 # my_fruit_list = my_fruit_list.set_index('Fruit')
-# Wrong statement fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list),  ['Avocado', 'Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 import requests
 streamlit.header('Acces json data and show ')
