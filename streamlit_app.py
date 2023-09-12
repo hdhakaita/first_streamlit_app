103502e00d7a8768a31c93a371b1ac8ea7c6df83
hdhakaita <78197458+hdhakaita@users.noreply.github.com>
2023-09-12 17:45:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,19 @@
 import streamlit
 streamlit.title('My parent New Healthy Diner')
-
 streamlit.header('🥣 Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑 🍞 Hard-Boiled Free-Range Egg')
-   
-#--streamlit.header('Breakfast Menu')
-#--streamlit.text('Hi This is Hemendra Dhakaita')
-#--streamlit.text('Learning Snowflake App development')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
-
-streamlit.dataframe(my_fruit_list)
-
+#streamlit.dataframe(my_fruit_list)
 #my_fruit_list = my_fruit_list.set_index('Fruit')
-
-
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list),  ['Avocado', 'Strawberries'])
-
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list),  ['Avocado', 'Strawberries'])
+#fruits_to_show = my_fruit_list.loc[fruits_selected]
