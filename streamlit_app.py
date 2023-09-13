fa6071d6c3dae285cc6fe20e16b9f8db40438ca5
hdhakaita <78197458+hdhakaita@users.noreply.github.com>
2023-09-13 17:50:20 +0530

    Update streamlit_app.py
    add function to code for code repetation

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,15 +40,21 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-streamlit.header('Fruityvice Fruit Advice !!!')
+#create repeatale code block called function
+def get_fruityvice_data(this_fruit_choice) 
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = padans.json_normalized(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+
+#new section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
 try: 
   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
   if not fruit_choice:
     streamlit.error("Please select fruit to get information")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = padans.json_normalized(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
   
