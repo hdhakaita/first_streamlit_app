aaef0835e7802dc8b3eb37ba054d413d06785725
hdhakaita <78197458+hdhakaita@users.noreply.github.com>
2023-09-13 17:57:37 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 #create repeatale code block called function
 def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = padans.json_normalized(fruityvice_response.json())
     return fruityvice_normalized
 #streamlit.dataframe(fruityvice_normalized)
