93cbbc759c750831ed01b19c84a2ef00be2477a0
hdhakaita <78197458+hdhakaita@users.noreply.github.com>
2023-09-13 18:35:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,19 +44,17 @@ streamlit.dataframe(fruityvice_normalized)
 #create repeatale code block called function
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    #fruityvice_normalized = padans.json_normalized(fruityvice_response.json())
-    #return fruityvice_normalized
-    return fruityvice_response
-
-
+    fruityvice_normalized = padans.json_normalized(fruityvice_response.json())
+    return fruityvice_normalized
+    
 #new section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice!')
+streamlit.header('Hemendra: Fruityvice Fruit Advice!')
 try: 
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select fruit to get information")
   else:
-    back_from_function = get_fruityvice_data(this_fruit_choice)
+    back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
