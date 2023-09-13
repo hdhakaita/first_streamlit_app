8e05938a138d1f172934339b6e8d78a45c492cbd
hdhakaita <78197458+hdhakaita@users.noreply.github.com>
2023-09-13 21:34:05 +0530

    Update streamlit_app.py
    indenting

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from fruit_load_list")
         my_data = my_cur.fetchall
-        return my_data
+    return my_data
 
 # Add abutton to load the fruit
 if streamlit.button('Get Fruit Load List'):
